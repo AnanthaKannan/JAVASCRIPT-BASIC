b3f442f7b947268e1de12c442a0d3ba7719f495a
SreeAnanthaKannnan <Sree.kannan@rapidqube.com>
2019-10-03 17:15:04 +0530

    graphql structure updated

--- graphql/app.js
+++ graphql/app.js
@@ -1,80 +1,11 @@
-const author =[
-  {id:1, name:'Ananth'},
-  {id:2, name:'kannan'},
-  {id:3, name:'sree'}
-]
-
-const books = [
-  {id:1, name:'tamil', auther_id:1},
-  {id:2, name:'english', auther_id:1},
-  {id:3, name:'maths', auther_id:1},
-  {id:4, name:'physics', auther_id:2},
-  {id:5, name:'accounts', auther_id:2},
-  {id:6, name:'bio', auther_id:3},
-  {id:7, name:'science', auther_id:3},
-  {id:8, name:'computer', auther_id:3},
-]
-
 const express = require('express');
 const expressGraphQl = require('express-graphql');
-const { GraphQLSchema, 
-  GraphQLObjectType, GraphQLNonNull,
-   GraphQLString, GraphQLList, GraphQLInt } = require('graphql');
-
+var cors = require('cors')
+const { GraphQLSchema } = require('graphql');
+const { RootQueryType } = require('./rootQuery');
 const app = express();
+app.use(cors())
 
-const BookType = new GraphQLObjectType({
-  name:'Book',
-  description:'This represents a book written by author',
-  fields:() =>({
-    id:{ type: GraphQLNonNull(GraphQLInt)},
-    name:{ type: GraphQLNonNull(GraphQLString)},
-    auther_id:{ type: GraphQLNonNull(GraphQLInt)}
-  })
-});
-
-const authorType = new GraphQLObjectType({
-  name:'Author',
-  description:'List of authors',
-  fields:() =>({
-    id:{ type: GraphQLNonNull(GraphQLInt)},
-    name:{ type: GraphQLNonNull(GraphQLString)}
-  })
-});
-
-const RootQueryType = new GraphQLObjectType({
-  name:'Query',
-  description:'Root Query',
-  fields:() =>({
-    books:{
-      type: new GraphQLList(BookType),
-      description:'List of all books',
-      resolve:() => books
-    },
-    author:{
-      type: new GraphQLList(authorType),
-      description:'List of all author',
-      resolve:() => author
-    },
-    book:{
-      type:BookType,
-      description: 'A single book',
-      args:{
-        id:{ type: GraphQLInt}
-      },
-      resolve: (parent, args) => books.find(book => book.id === args.id)
-    },
-    booksPagination:{
-      type: new GraphQLList(BookType),
-      description:'Give a page name and limits',
-      args:{
-        page: { type: GraphQLInt},
-        limit: { type: GraphQLInt}
-      },
-      resolve:(parent, args) => books.slice(((args.page -1) * args.limit), (args.page * args.limit) )
-    } 
-  })
-})
 
 const schema = new GraphQLSchema({
   query: RootQueryType
@@ -82,7 +13,7 @@ const schema = new GraphQLSchema({
 
 app.use('/graphql', expressGraphQl({
   schema:schema,
-  graphiql:true
+  graphiql:true,
  
 }))
 
---
+++ graphql/fields.js
@@ -0,0 +1,37 @@
+const { GraphQLSchema, 
+    GraphQLObjectType, GraphQLNonNull,
+     GraphQLString, GraphQLList, GraphQLInt } = require('graphql');
+
+const BookType = new GraphQLObjectType({
+    name:'Book',
+    description:'This represents a book written by author',
+    fields:() =>({
+      id:{ type: GraphQLNonNull(GraphQLInt)},
+      name:{ type: GraphQLNonNull(GraphQLString)},
+      auther_id:{ type: GraphQLNonNull(GraphQLInt)}
+    })
+  });
+  
+  const authorType = new GraphQLObjectType({
+    name:'Author',
+    description:'List of authors',
+    fields:() =>({
+      id:{ type: GraphQLNonNull(GraphQLInt)},
+      name:{ type: GraphQLNonNull(GraphQLString)}
+    })
+  });
+
+  const statusType = new GraphQLObjectType({
+      name:'Status',
+      description:'Status of the query',
+      fields:() =>({
+          message:{ type: GraphQLNonNull(GraphQLString)},
+          msgCode: { type: GraphQLNonNull(GraphQLString)},
+          statusCode: { type: GraphQLNonNull(GraphQLInt) }
+      })
+  })
+
+
+  module.exports = {
+      BookType, authorType, statusType
+  }
