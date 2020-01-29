b88bc600f5e1fa980328227705012a2e4e1af592
SreeAnanthaKannnan <Sree.kannan@rapidqube.com>
2020-01-29 15:35:00 +0530

    added

---
+++ websocket/socketClient.js
@@ -0,0 +1,11 @@
+const socket = require('socket.io-client')('http://localhost:3000');
+socket.on('connect', function(){
+    console.log('connect')
+});
+socket.on('event', function(data){
+    console.log('event')
+});
+socket.on('disconnect', function(){
+    console.log('disconnect')
+
+});
